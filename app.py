c0a188a6f943327613ec5eeb3a2bcc9739796fb8
Paras Varshney <paras.varshney97@gmail.com>
2020-04-23 11:29:52 +0530

    v1.0 available

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ Created on Wed Apr 22 06:43:26 2020
 from flask import Flask, jsonify, request
 import pandas as pd
 import numpy as np
+import json
 from sklearn.externals import joblib 
 
 
@@ -20,19 +21,20 @@ import flask
 app = Flask(__name__)
 
 main_cols = joblib.load("columns.pkl")
+main_cols = joblib.load("columns.pkl")
+    
 
-def clean_data(data):
-    data.loc[:, ["Gender"]] = gender["Female"]
-    columnTransformer = ColumnTransformer([('encoder', OneHotEncoder(), [1])], remainder='passthrough')
-    data = np.array(columnTransformer.fit_transform(data), dtype = np.str)
-    data = data[:, 1:]
-    return data
+def clean_data(df_x):
+    le = LabelEncoder()
+    df_x.Gender = le.fit_transform(df_x.Gender)
+    df_x = pd.get_dummies(data = df_x,  columns=["Geography"], drop_first = False)
+    return df_x
 
 
-def standardize_data(data):
-    scaler = StandardScaler()
-    data = scaler.fit_transform(data)
-    return data
+def standardize_data(dta):
+    scaler = joblib.load("std_scaler.pkl")
+    X_transformed = scaler.transform(dta)
+    return X_transformed
 
 
 @app.route('/')
@@ -52,24 +54,27 @@ def predict():
     
     df_input = pd.DataFrame.from_records([form_data], )
     df_input = df_input.drop(['submitBtn'], axis=1)
-    print(type(df_input))
+    df_input = pd.DataFrame(df_input)
+ 
+    sample_df = pd.DataFrame(columns = main_cols)
+    clean_df = clean_data(df_input)
+    main_df = sample_df.append(clean_df)
+    main_df = main_df.fillna(0)
+    print(main_df)
     
-    main_df = pd.DataFrame(columns=main_cols)
-    print(main_df)
-    main_df = main_df.append(df_input)
-    print(main_df)
-   
+         
+    std_df = standardize_data(main_df)
+    print(std_df)
     
     clf = joblib.load('prediction_classifier.pkl')
-    clean_df = clean_data(df_input)
-    std_df = standardize_data(clean_df)
     pred = clf.predict(std_df)
+    print(pred, pred[0], pred[0][0])
+    x = pred[0][0]
+    
+    print(x)
+    
+    return jsonify({'prediction': x})
 
-
-    return jsonify({'prediction': pred})
-
-
-main_cols = joblib.load("columns.pkl")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
--- model.py
+++ model.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
 # -*- coding: utf-8 -*-
 """
 Created on Wed Apr 22 05:42:28 2020
@@ -9,56 +13,93 @@ import pandas as pd
 import numpy as np
 from sklearn.externals import joblib 
 
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+
 df = pd.read_csv("Churn_Modelling.csv")
 
 df_x = df.iloc[:, 3:13]
 df_y = df.iloc[:, 13]
 
-main_cols = df_x.columns
-joblib.dump(main_cols, "columns.pkl")
+df_x.head()
 
 # df.isna().sum()
 
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-from sklearn.compose import ColumnTransformer
 
-encoder = LabelEncoder()
-df_x.iloc[:, 2] = encoder.fit_transform(df_x.iloc[:, 2])
 
-columnTransformer = ColumnTransformer([('encoder', OneHotEncoder(), [1])], remainder='passthrough')
-col_tnf = columnTransformer.fit_transform(df_x)
-df_x = np.array(col_tnf, dtype = np.str)
-df_x = df_x[:, 1:]
+def clean_data(df):
+
+    le = LabelEncoder()
+    df.Gender = le.fit_transform(df.Gender)
+    df = pd.get_dummies(data = df, columns=["Geography"], drop_first = False)
+    df = df.sort_index(axis=1)
+    return df
+
+
+
+
+df_x = clean_data(df_x)
+# df_x.head()
+
+
+
+# df_one = pd.DataFrame(df_x.iloc[2, :])
+# df_one = df_one.T
+# df_one
+# clean_data(df_one)
+
+
+
+
+
+# columnTransformer = ColumnTransformer([('encoder', OneHotEncoder(), [1])], remainder='passthrough')
+# col_tnf = columnTransformer.fit_transform(df_x)
+# df_x = np.array(col_tnf, dtype = np.str)
+# df_x = df_x[:, 1:]
 
 
 
 from sklearn.model_selection import train_test_split
+
 X_train, X_test, y_train, y_test = train_test_split(df_x, df_y, test_size = 0.2, random_state = 0)
+
+joblib.dump(X_train.columns, "columns.pkl")
+
+
 
 from sklearn.preprocessing import StandardScaler
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
+joblib.dump(scaler, "std_scaler.pkl")
+print(X_test)
+print(X_train.shape[1])
+
+
 
 import keras
 from keras.models import Sequential
 from keras.layers import Dense
 
 classifier = Sequential()
-classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu', input_dim = 11))
+classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu', input_dim = X_train.shape[1]))
 classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu'))
 classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
 classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
-classifier.fit(X_train, y_train, batch_size = 10, epochs = 100)
+classifier.fit(X_train, y_train, batch_size = 10, epochs = 25)
+
 
 
 y_pred = classifier.predict(X_test)
+print(y_pred)
 y_pred = (y_pred > 0.5)
 
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
 accuracy = (cm[0,0]+cm[1, 1])/(cm[0,0]+cm[1, 1]+cm[1,0]+cm[0, 1])
-
+print(accuracy)
 joblib.dump(classifier, 'prediction_classifier.pkl') 
+
+
 
